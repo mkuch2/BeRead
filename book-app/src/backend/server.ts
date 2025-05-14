0534717c8c681f7b0719c539c49d859390ed2cbc
kuchimpos <kuchimpos@g.ucla.edu>
2025-05-13 18:55:22 -0700

    Configure routing to static files

--- book-app/src/backend/server.ts
+++ book-app/src/backend/server.ts
@@ -2,16 +2,26 @@ import express, {
   type Application,
   type Request,
   type Response,
-  type NextFunction,
 } from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app: Application = express();
 const PORT: string | number = process.env.PORT || 5004;
 
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response): void => {
-  res.send("<h1>Hello, this is my website!</h1>");
+//Serve all files from dist folder as static assets / files
+app.use(express.static(path.join(__dirname, "..", "..", "dist")));
+
+//API Routes
+
+//Non-API Routes
+app.all("/", (_req: Request, res: Response): void => {
+  res.sendFile(path.join(__dirname, "..", "..", "dist", "index.html"));
 });
 
 app.listen(PORT, (): void => {
