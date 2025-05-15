4b582ab8b756c9838f4feac4261f116f24fa121e
kuchimpos <kuchimpos@g.ucla.edu>
2025-05-15 13:52:55 -0700

    Add and configure tailwindcss and shadcn/ui

--- src/frontend/pages/Landing.tsx
+++ src/frontend/pages/Landing.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router";
+import { Button } from "../components/ui/button";
 
 function Landing() {
   return (
@@ -6,6 +7,9 @@ function Landing() {
       <p>This is the landing page</p>
       <Link to="/login">Login Page</Link>
       <Link to="/signup">Signup Page</Link>
+
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button>Shad Button</Button>
     </>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,22 @@
+import path from "path";
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       "/api": {
         target: "http://localhost:5004",
         changeOrigin: true,
       },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
