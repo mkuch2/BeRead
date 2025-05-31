d51bf0401ced1ebe661e9fc322f4b1af10b05625
leechristianjm <160551443+leechristianjm@users.noreply.github.com>
2025-05-30 20:36:15 -0700

    Update landing
    Re-add friends and profile tab

--- src/frontend/pages/Landing.tsx
+++ src/frontend/pages/Landing.tsx
@@ -7,6 +7,8 @@ function Landing() {
         <div className="flex items-center space-x-4">
           <h1 className="font-bold text-xl text-white">BeRead</h1>
           <Link to="/books" className="text-sm font-light text-zinc-400">Search Books</Link>
+          <Link to="/friends" className="text-sm text-zinc-400 hover:text-white transition">Friends</Link>
+          <Link to="/profile" className="text-sm text-zinc-400 hover:text-white transition">Profile</Link>
         </div>
         <Link to="/login" className="text-sm font-light text-zinc-400">Login</Link>
       </header>
